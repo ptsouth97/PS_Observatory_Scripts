2352201719a6e1a1479f2a62d8dacdf6572ad680
Crosby <Blake.Crosby@tridenttech.edu>
2025-01-28 10:48:04 -0500

    added file

---
+++ PS_Observatory_Scripts/CCD_Camera_Control.ps1
@@ -0,0 +1,15 @@
+﻿$cam = New-Object -ComObject "MaxIm.CCDCamera"
+$cam.LinkEnabled = $true
+
+Write-Output "Camera is ready, Exposing."
+
+#while (-not $cam.ImageReady) {
+#    $cam.StartExposure(1, 1)
+#}
+
+$cam.Expose(1, 1, 0)
+
+Start-Sleep -Seconds 5
+
+
+$cam.SaveImage("C:\Users\BlakeCrosby\Documents\Image.fit")
