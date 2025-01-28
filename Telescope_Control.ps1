3c87b67b7f850796f765ae66d7e40da1d3352b91
ptsouth97 <ptsouth97@gmail.com>
2025-01-28 14:26:21 -0500

    added Telescope Control

---
+++ Telescope_Control.ps1
@@ -0,0 +1,14 @@
+﻿$telescope = New-Object -ComObject "ASCOM.Simulator.Telescope"
+
+Write-Output "The telescope's aperture in meters is:"
+$telescope.ApertureDiameter
+
+Write-Output "The telescope's focal length in meters is:"
+$telescope.FocalLength
+
+Write-Output "The driver version is:"
+$telescope.DriverVersion
+
+$telescope.Tracking = $true
+
+$telescope.SlewToCoordinates(15.1, 56.2)
